51b3fc296009aff2ca59bff21cd1cd9d42e383e3
Laura Abend <laura.l.abend@gmail.com>
2016-09-04 23:50:04 -0500

    add js file

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function splinter(a,b) {
+  var z = a*b;
+  var testArray = [a, b, z];
+for(i = 0; i < testArray.length; i++) {
+  if (i <= 3) {
+  return testArray;
+  }
+  else {
+    return ("nope");
+  }
+}
+}
